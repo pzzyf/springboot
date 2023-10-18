c68a5c29e0357c98dd85a15505427f6fa856f51e
afe1 <2279948211@qq.com>
2023-10-18 17:07:51 +0800

    hello word

---
+++ src/main/java/afe1/controller/HelloController.java
@@ -0,0 +1,13 @@
+package afe1.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @RequestMapping("/hello")
+    public String hello() {
+        return "hello word";
+    }
+}
